db8e704859456ab41e1a5967b8e918d1e1a9c921
h11c <humaid_ahmed98@outlook.com>
2026-01-09 19:57:33 +0000

    Create level04.sh

---
+++ levels/level04.sh
@@ -0,0 +1,13 @@
+### Bash Battle Arena – Level 4: File Manipulation
+
+**Concepts Learned**
+- File globbing with `*.txt`
+- Directory creation using `mkdir -p`
+- Copying files with `cp`
+
+**Script**
+```bash
+#!/bin/bash
+
+mkdir -p Backup
+cp *.txt Backup/
